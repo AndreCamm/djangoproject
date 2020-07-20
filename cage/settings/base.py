78f45721c7046fe1cb6a15aaa90fbd27308bf23f
Robert <info@jmkit.com>
2020-07-20 15:04:23 +0100

    restructured for heroku

--- cage/settings/base.py
+++ cage/settings/base.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%3l(zj#wquek-8pfxqmn!2_1^8qfi@ho7lsh4624usw&m6d-+!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp.com"]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 MEDIA_URL = '/uploads/'
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
