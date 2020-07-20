fbcb37df3f486111f9b2fa0e215232507b645614
Robert <info@jmkit.com>
2020-07-20 16:51:52 +0100

    latest updates

--- cage/settings/base.py
+++ cage/settings/base.py
@@ -19,11 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%3l(zj#wquek-8pfxqmn!2_1^8qfi@ho7lsh4624usw&m6d-+!'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = [".herokuapp.com"]
 
