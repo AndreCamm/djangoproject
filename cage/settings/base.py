d3178c7a864d7088610443d24bf4b82ce6889804
Robert <info@jmkit.com>
2020-07-21 14:36:26 +0100

    moved static files

--- cage/settings/base.py
+++ cage/settings/base.py
@@ -117,5 +117,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 LOGIN_REDIRECT_URL = '/subjects'
